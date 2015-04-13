9e94d9672c54b353ccf712e401401398e3cecfe8
Carl Danley <carldanley@gmail.com>
2015-04-13 11:24:45 -0400

    syncing

---
+++ tests/src/events.js
@@ -0,0 +1,13 @@
+// external dependencies
+const chai = require('chai');
+const expect = chai.expect;
+const _ = require('lodash');
+
+// internal dependencies
+const githubWebhookMappings = require('../../src/mappings.json');
+const GitHubWebhooks = require('../../src/github-webhooks');
+
+it('emits a "commit-comment" event', function() {
+  //
+});
+
